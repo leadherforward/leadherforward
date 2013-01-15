e3d974a52c95fcd1c4c248abd32d1436e944d0c1
Jon Conley <conley.jj@gmail.com>
2013-01-15 18:03:07 -0500

    Updated json path for timeline

--- public/javascripts/timeline_ourconfigs.js
+++ public/javascripts/timeline_ourconfigs.js
@@ -1,9 +1,7 @@
 var timeline_config = {
-      width: '100%',
-      height:  '400',
-      source:  'example_json.json',
-      embed_id: 'timeline-embed',
-      start_at_end: true
-      };
-
-
+  width: '100%',
+  height:  '400',
+  source:  'javascripts/example_json.json',
+  embed_id: 'timeline-embed',
+  start_at_end: true
+};
