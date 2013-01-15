56d0148901c8296c3e50c8a9ea0a6fe00b3f9df1
Jon Conley <conley.jj@gmail.com>
2013-01-14 19:00:58 -0500

    Load the profile page via ajax and show as a modal

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,11 @@
 jQuery(document).ready(function($) {
 
   // Script
-
+ $('#example-profile').click(function(event){
+   event.preventDefault();
+   $.get( this.href, function(data){
+     $(data).modal('show');
+   });
+ });
 
 });
