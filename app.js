ab14594dde21646043b5b4a8bcbf5a10d60be8ee
Jon Conley <conley.jj@gmail.com>
2013-01-18 16:16:10 -0500

    Removed index.html and updated title

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.set('title', 'LeadHerForward');
+  app.set('title', 'Lead Her Forward');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
