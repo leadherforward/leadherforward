1d7a00c9c4ece5750dd6fa9a8d2e266ae125eefe
Jon Conley <conley.jj@gmail.com>
2013-01-17 23:51:38 -0500

    Updated import to handle photo format

--- import.js
+++ import.js
@@ -6,11 +6,25 @@ fs.readFile('raw_profile_data.txt', 'utf8', function(err, data){
   var raw_profiles = data.split('--profile--');
   $.each(raw_profiles, function(index, raw_profile){
     var raw_fields = raw_profile.trim().split('---');
-    var profile = {};
+    var profile = {
+      photos: [],
+      photo_source_names: [],
+      photo_sources: []
+    };
     $.each(raw_fields, function(index, raw_field){
       var field_name = raw_field.trim().match(/^((?:\w+|\s*)+):/m)[1].toLowerCase().replace(/\s+/, '_');
       var field_content = raw_field.replace(/^((?:\w+|\s*)+):/m, '').trim();
-      profile[field_name] = field_content;
+      switch (field_name)
+      {
+        case 'Picture1':
+          photo_content = field_content.split(',');
+          profile.photos.push(photo_content[0].trim());
+          profile.photo_source_names.push(photo_content[1].trim());
+          profile.photo_sources.push(photo_content[2].trim());
+        break;
+        default:
+          profile[field_name] = field_content;
+      }
     });
     profiles.push(profile);
   });
