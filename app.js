8a840f1b9360f4c1c66af3c66b811f71d079009b
ranakhalil <rana.a.khalil@disney.com>
2013-01-17 20:41:06 -0800

    Update app.js

--- app.js
+++ app.js
@@ -55,8 +55,6 @@ app.get('/take_the_lead/:id', lead.list);
 app.get('/team', info.team);
 app.get('/why', info.why);
 app.get('/about', info.about);
-app.get('/resources', info.resources);
-app.get('/faq', info.faq);
 app.get('/contact_us', info.contact_us);
 
 http.createServer(app).listen(app.get('port'), function(){
