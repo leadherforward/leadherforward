96e45e1ee6229ef7164c797ae7adabdd7e93263d
Jon Conley <conley.jj@gmail.com>
2013-01-20 00:55:46 -0500

    Moved and renamed example_json.json

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,3 @@
-//var converted = JSON.parse('example_json.json');
-
 jQuery(document).ready(function($) {
   setAltInTimeline();
   // Script
--- public/javascripts/timeline_ourconfigs.js
+++ public/javascripts/timeline_ourconfigs.js
@@ -1,7 +1,7 @@
 var timeline_config = {
   width: '100%',
   height:  '600',
-  source:  '/javascripts/example_json.json',
+  source:  '/timeline_config/config.json',
   embed_id: 'timeline-embed',
   start_at_end: false
 };
